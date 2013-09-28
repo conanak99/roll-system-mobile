451b5315e742472b69d989a972dd8c49d68efe09
hoangphse60740@fpt.edu.vn <hoangphse60740@fpt.edu.vn@53922ad7-7c9d-c338-3f44-60befd1a0cec>
2013-09-28 07:50:28 +0000

    Code xong, chua chạy. Khi nao chay biet lien.

--- trunk/Code/RSM/RollSystemMobile/Models/ViewModels/InstructorViewModel.cs
+++ trunk/Code/RSM/RollSystemMobile/Models/ViewModels/InstructorViewModel.cs
@@ -13,4 +13,11 @@ namespace RollSystemMobile.Models.ViewModels
         public RollCall CurrentRollCall { get; set; }
         public IEnumerable<RollCall> TeachingRollCall { get; set; }
     }
+
+    public class AttendanceViewModel
+    {
+        public List<RecognizerResult> RecognizeResult { get; set; }
+        public List<Student> PresentStudents { get; set; }
+        public RollCall CurrentRollCall { get; set; }
+    }
 }
