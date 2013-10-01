3381d8330db93b0cb108d11c6bb1abf87c39af38
hoangphse60740@fpt.edu.vn <hoangphse60740@fpt.edu.vn@53922ad7-7c9d-c338-3f44-60befd1a0cec>
2013-10-01 07:05:11 +0000

    Fix Some Roll Call Logic.

--- trunk/Code/RSM/RollSystemMobile/Controllers/RollCallController.cs
+++ trunk/Code/RSM/RollSystemMobile/Controllers/RollCallController.cs
@@ -6,13 +6,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RollSystemMobile.Models;
+using System.Drawing;
 
 namespace RollSystemMobile.Controllers
-{ 
+{
     public class RollCallController : Controller
     {
         private RSMEntities db = new RSMEntities();
-
         //
         // GET: /RollCall/
 
@@ -36,40 +36,77 @@ namespace RollSystemMobile.Controllers
 
         public ActionResult Create()
         {
-            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName");
-            ViewBag.SemesterID = new SelectList(db.Semesters, "SemesterID", "SemesterName");
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "ShortName");
+            //Mac dinh, majorID la 1
+            int MajorID = 1;
+
+            //Them 1 cai ID cua instructor
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "FullName");
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "FullName", MajorID);
+            ViewBag.ClassID = new SelectList(db.Classes.Where(c => c.MajorID == MajorID),
+                "ClassID", "ClassName");
+            //Mac dinh, lay semester moi nhat
+            ViewBag.SemesterID = new SelectList(db.Semesters, "SemesterID", "SemesterName", db.Semesters.ToList().Last().SemesterID);
+            ViewBag.SubjectID = new SelectList(db.Majors.
+                Single(m => m.MajorID == MajorID).Subjects,
+                "SubjectID", "ShortName");
+
             return View();
-        } 
+        }
 
         //
         // POST: /RollCall/Create
 
         [HttpPost]
-        public ActionResult Create(RollCall rollcall)
+        public ActionResult Create(RollCall rollcall, int MajorID, int InstructorID)
         {
             if (ModelState.IsValid)
             {
-                db.RollCalls.AddObject(rollcall);
-                db.SaveChanges();
-                return RedirectToAction("Index");  
+                RollCallBO BO = new RollCallBO();
+                List<string> ErrorList = BO.ValidRollCall(rollcall);
+                if (ErrorList.Count == 0)
+                {
+                    rollcall = BO.FillRollCallInfo(rollcall, InstructorID);
+                    BO.InsertRollCall(rollcall);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var Error in ErrorList)
+                    {
+                        ModelState.AddModelError(String.Empty, Error);
+                    }
+                }
             }
 
-            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", rollcall.ClassID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "FullName", InstructorID);
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "FullName", MajorID);
+            ViewBag.ClassID = new SelectList(db.Classes.Where(c => c.MajorID == MajorID),
+                "ClassID", "ClassName", rollcall.ClassID);
             ViewBag.SemesterID = new SelectList(db.Semesters, "SemesterID", "SemesterName", rollcall.SemesterID);
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "ShortName", rollcall.SubjectID);
+            ViewBag.SubjectID = new SelectList(db.Majors.
+                Single(m => m.MajorID == MajorID).Subjects,
+                "SubjectID", "ShortName", rollcall.SubjectID);
             return View(rollcall);
         }
-        
+
         //
         // GET: /RollCall/Edit/5
- 
+
         public ActionResult Edit(int id)
         {
             RollCall rollcall = db.RollCalls.Single(r => r.RollCallID == id);
-            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", rollcall.ClassID);
+
+            int MajorID = rollcall.Class.MajorID;
+            int InstructorID = rollcall.InstructorTeachings.ToList().Last().InstructorID;
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "FullName", MajorID);
+            ViewBag.ClassID = new SelectList(db.Classes.Where(c => c.MajorID == MajorID),
+                "ClassID", "ClassName", rollcall.ClassID);
             ViewBag.SemesterID = new SelectList(db.Semesters, "SemesterID", "SemesterName", rollcall.SemesterID);
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "ShortName", rollcall.SubjectID);
+            ViewBag.SubjectID = new SelectList(db.Majors.
+                Single(m => m.MajorID == MajorID).Subjects,
+                "SubjectID", "ShortName", rollcall.SubjectID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "InstructorID", "FullName", InstructorID);
+
             return View(rollcall);
         }
 
@@ -77,24 +114,43 @@ namespace RollSystemMobile.Controllers
         // POST: /RollCall/Edit/5
 
         [HttpPost]
-        public ActionResult Edit(RollCall rollcall)
+        public ActionResult Edit(RollCall rollcall, int InstructorID)
         {
             if (ModelState.IsValid)
             {
-                db.RollCalls.Attach(rollcall);
-                db.ObjectStateManager.ChangeObjectState(rollcall, EntityState.Modified);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                RollCallBO BO = new RollCallBO();
+                List<string> ErrorList = BO.ValidRollCall(rollcall);
+                if (ErrorList.Count == 0)
+                {
+                    // Kiem tra xem co doi giao vien hay ko
+                    rollcall = BO.ChangeRollCallInstructor(rollcall, InstructorID);
+                    BO.UpdateRollCall(rollcall);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var Error in ErrorList)
+                    {
+                        ModelState.AddModelError(String.Empty, Error);
+                    }
+                }
             }
-            ViewBag.ClassID = new SelectList(db.Classes, "ClassID", "ClassName", rollcall.ClassID);
+
+            int MajorID = rollcall.Class.MajorID;
+            ViewBag.MajorID = new SelectList(db.Majors, "MajorID", "FullName", MajorID);
+            ViewBag.ClassID = new SelectList(db.Classes.Where(c => c.MajorID == MajorID),
+                "ClassID", "ClassName", rollcall.ClassID);
             ViewBag.SemesterID = new SelectList(db.Semesters, "SemesterID", "SemesterName", rollcall.SemesterID);
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "ShortName", rollcall.SubjectID);
+            ViewBag.SubjectID = new SelectList(db.Majors.
+                Single(m => m.MajorID == MajorID).Subjects,
+                "SubjectID", "ShortName", rollcall.SubjectID);
+
             return View(rollcall);
         }
 
         //
         // GET: /RollCall/Delete/5
- 
+
         public ActionResult Delete(int id)
         {
             RollCall rollcall = db.RollCalls.Single(r => r.RollCallID == id);
@@ -103,11 +159,21 @@ namespace RollSystemMobile.Controllers
 
         //
         // POST: /RollCall/Delete/5
-
+        //Sau nay ko dung ham nay, chi dung trong giai doan test
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
-        {            
+        {
             RollCall rollcall = db.RollCalls.Single(r => r.RollCallID == id);
+            foreach (var InstructorTeaching in rollcall.InstructorTeachings.ToList())
+            {
+                db.InstructorTeachings.DeleteObject(InstructorTeaching);
+            }
+
+            foreach (var Student in rollcall.Students.ToList())
+            {
+                rollcall.Students.Remove(Student);
+            }
+
             db.RollCalls.DeleteObject(rollcall);
             db.SaveChanges();
             return RedirectToAction("Index");
@@ -117,6 +183,24 @@ namespace RollSystemMobile.Controllers
         {
             db.Dispose();
             base.Dispose(disposing);
+        }
+
+        //Lay danh sach class cua 1 major, de dua vao dropbox
+        public JsonResult GetClasses(int id)
+        {
+            var Classes = db.Classes.Where(c => c.MajorID == id).
+                Where(c => c.IsActive).OrderBy(c => c.ClassName).
+                Select(c => new { id = c.ClassID, name = c.ClassName });
+            return Json(Classes, JsonRequestBehavior.AllowGet);
+        }
+
+        //Lay danh sach mon hoc cua 1 major, de dua vao dropbox
+        public JsonResult GetSubjects(int id)
+        {
+            var Subject = db.Majors.Single(m => m.MajorID == id).Subjects.
+                Where(s=> s.IsActive).OrderBy(s => s.ShortName).
+                Select(s => new { id = s.SubjectID, name = s.ShortName });
+            return Json(Subject, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ trunk/Code/RSM/RollSystemMobile/Models/RollCallBO.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RollSystemMobile.Models.BindingModels;
+
+namespace RollSystemMobile.Models
+{
+    public class RollCallBO
+    {
+        private RSMEntities db;
+
+        public RollCallBO()
+        {
+            db = new RSMEntities();
+        }
+
+        public void InsertRollCall(RollCall InRollCall)
+        {
+            db.RollCalls.AddObject(InRollCall);
+            db.SaveChanges();
+        }
+
+        public void UpdateRollCall(RollCall InRollCall)
+        {
+            db.RollCalls.Attach(InRollCall);
+            db.ObjectStateManager.ChangeObjectState(InRollCall, System.Data.EntityState.Modified);
+            db.SaveChanges();
+        }
+
+        public RollCall ChangeRollCallInstructor(RollCall InRollCall, int NewInstructorID)
+        {
+            RollCall rollcall = InRollCall;
+            var RollCallInstructorTeaching = db.InstructorTeachings.
+                Where(it => it.RollCallID == InRollCall.RollCallID).ToList();
+
+            int CurrentInstructorID = RollCallInstructorTeaching.Last().InstructorID;
+            //Neu co doi giao vien, doi gia tri field cu, them field moi
+            if (NewInstructorID != CurrentInstructorID)
+            {
+                InstructorTeaching it = RollCallInstructorTeaching.Last(inte => inte.InstructorID == CurrentInstructorID);
+
+                //Neu nhu doi giao vien sau start date thi ghi log
+                //SAU NAY KO CHECK START DATE, CHECK BANG GIA TRI STATUS 0,1,2
+                if (DateTime.Today > it.BeginDate)
+                {
+                    it.EndDate = DateTime.Today.AddDays(-1); //Ngay ket thuc la hom qua
+                    
+                    InstructorTeaching newIt = new InstructorTeaching();
+                    newIt.InstructorID = NewInstructorID;
+                    newIt.BeginDate = DateTime.Today;
+                    newIt.EndDate = rollcall.EndDate;
+                    newIt.RollCallID = rollcall.RollCallID;
+                    db.InstructorTeachings.AddObject(newIt);
+                }
+                else
+                {
+                    //Neu truoc start date, chi doi bt
+                    it.InstructorID = NewInstructorID;
+                }
+
+            }
+
+            return rollcall;
+        }
+
+
+        public RollCall FillRollCallInfo(RollCall InRollCall, int InstructorID)
+        {
+            RollCall rollcall = InRollCall;
+            //Set thoi gian EndTime, dua vao start time
+            var rollSubject = db.Subjects.First(s => s.SubjectID == rollcall.SubjectID);
+            rollcall.EndTime = rollcall.StartTime.Add(TimeSpan.FromMinutes(45 * rollSubject.NumberOfSlot));
+
+            //Dua toan bo hoc sinh hien tai cua class vao
+            var rollClass = db.Classes.First(c => c.ClassID == rollcall.ClassID);
+            foreach (var Student in rollClass.Students)
+            {
+                rollcall.Students.Add(Student);
+            }
+
+            // Them 1 truong vao bang instructor teaching
+            InstructorTeaching it = new InstructorTeaching();
+            it.InstructorID = InstructorID;
+            it.BeginDate = rollcall.BeginDate;
+            it.EndDate = rollcall.EndDate;
+            rollcall.InstructorTeachings.Add(it);
+            rollcall.Status = 0;
+
+            return rollcall;
+
+        }
+
+
+        public List<String> ValidRollCall(RollCall InRollCall)
+        {
+            List<string> ErrorList = new List<string>();
+
+            var rollSubject = db.Subjects.First(s => s.SubjectID == InRollCall.SubjectID);
+            //Check may cai nhu gio hoc, giao vien dang day v...v trong nay
+            if ((InRollCall.StartTime.ToString(@"hh\:mm") == "10:45:"
+                || InRollCall.StartTime.ToString(@"hh\:mm") == "16:00") &&
+            rollSubject.NumberOfSlot == 2)
+            {
+                ErrorList.Add("Mon nay 2 slot, gio nay ko phu hop");
+            }
+
+            //Check thu xem class cua roll call nay co dang hoc roll call nao ko
+
+
+            //Check xem giao vien roll call nay co dang day roll call nao cung gio ko
+
+
+            //Nho check luon, start date va end date cua roll call so voi semester
+            return ErrorList;
+        }
+    }
+}
