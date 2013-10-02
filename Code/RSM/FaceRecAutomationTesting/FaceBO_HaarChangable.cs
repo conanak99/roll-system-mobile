f35ae462d02f5ebe4f659345b3f6694cb5189b32
Phạm Huy Hoàng <huyhoang8a5@gmail.com>
2013-10-02 11:28:27 +0000

    Update lại phần testing.

---
+++ Code/RSM/FaceRecAutomationTesting/FaceBO_HaarChangable.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace FaceRecAutomationTesting
+{
+
+    public class FaceBO_HaarChangable
+    {
+        public static HaarCascade Haar;
+        //Luc add, co the dung alt2
+        //Luc detect, nen dung alt_tree neu can chinh xac cao, can nhieu van dung alt2
+        private static String HaarXML = "haarcascade_frontalface_alt_tree.xml"; //"haarcascade_frontalface_alt2.xml"
+        private static double DETECT_SCALE = 1.1;
+        private static int MIN_NEIGHBOR = 3;
+        private static int MIN_SIZE = 20;
+        private static String HAAR_XML_PATH;
+        private static String TRAINING_FOLDER_PATH;
+
+        private static int RESIZE_WIDTH = 1300;
+        private static int RESIZE_HEIGHT = 800;
+
+
+        public static void SetXMLPath(String FilePath)
+        {
+            HAAR_XML_PATH = FilePath;
+        }
+
+        public static void SetTrainingFolderPath(String FolderPath)
+        {
+            TRAINING_FOLDER_PATH = FolderPath;
+        }
+
+        public static RecognizerResult DetectFromImage(string ImagePath)
+        {
+            RecognizerResult Result = new RecognizerResult();
+            //Lay moi ten anh, ko lay toan bo duong dan
+            Result.ImageLink = System.IO.Path.GetFileName(ImagePath);
+
+            if (Haar == null)
+            {
+                Haar = CreateHaar(HaarType.AltTree);
+            }
+
+            //Chuyen anh trang den roi bat dau recognize, dung using de tu giai phong memories
+            using (Image<Gray, byte> Image = new Image<Bgr, byte>(ImagePath).Convert<Gray, byte>())
+            {
+                var FacesDetected = Image.DetectHaarCascade(Haar, DETECT_SCALE, MIN_NEIGHBOR,
+                                    0, new System.Drawing.Size(MIN_SIZE, MIN_SIZE))[0];
+                foreach (var Face in FacesDetected)
+                {
+                    FaceRegion FaceReg = new FaceRegion(Face.rect.X, Face.rect.Y,
+                                    Face.rect.Width, Face.rect.Height);
+                    Result.FaceList.Add(FaceReg);
+                }
+
+            }
+
+            return Result;
+        }
+
+        //Resize lai anh
+        public static void ResizeImage(string OldPath, string NewPath)
+        {
+            using (Image<Bgr, byte> OldImage = new Image<Bgr, byte>(OldPath))
+            {
+                //Neu anh nho, 1 trong 2 chieu < max thi ko can resize
+                if (OldImage.Width < RESIZE_WIDTH || OldImage.Height < RESIZE_HEIGHT)
+                {
+                    OldImage.Save(NewPath);
+                }
+                else
+                {
+                    OldImage.Resize(RESIZE_WIDTH, RESIZE_HEIGHT, INTER.CV_INTER_CUBIC, true).Save(NewPath);
+                }
+            }
+        }
+
+
+        public static HaarCascade CreateHaar(HaarType HaarType)
+        {
+            switch (HaarType)
+            {
+                case HaarType.Alt1:
+                    return new HaarCascade(HAAR_XML_PATH + "/haarcascade_frontalface_alt.xml");
+                    break;
+                case HaarType.Alt2:
+                    return new HaarCascade(HAAR_XML_PATH + "/haarcascade_frontalface_alt2.xml");
+                    break;
+                case HaarType.AltTree:
+                    String Path = HAAR_XML_PATH + "/haarcascade_frontalface_alt_tree.xml";
+                    return new HaarCascade(HAAR_XML_PATH + "/haarcascade_frontalface_alt_tree.xml");
+                    break;
+                case HaarType.Default:
+                    return new HaarCascade(HAAR_XML_PATH + "/haarcascade_frontalface_default.xml");
+                    break;
+                default:
+                    break;
+            }
+            return null;
+        }
+
+        /*
+        public static void SaveTrainingData(string ImagePath, int FaceID, int StudentID)
+        {
+            //O ngoai ko check, ko save
+            if (FaceID == -1)
+            {
+                return;
+            }
+
+            RSMEntities db = new RSMEntities();
+
+            Image<Bgr, byte> Image = new Image<Bgr, byte>(ImagePath);
+            using (Image<Gray, byte> GrayImage = Image.Clone().Convert<Gray, byte>())
+            {
+                var FacesDetected = GrayImage.DetectHaarCascade(Haar, DETECT_SCALE, MIN_NEIGHBOR,
+                                    0, new System.Drawing.Size(MIN_SIZE, MIN_SIZE))[0];
+                Image<Bgr, byte> FaceImage = Image.Copy(FacesDetected[FaceID].rect)
+                                             .Resize(TRAINING_DATA_SIZE, TRAINING_DATA_SIZE,
+                                             INTER.CV_INTER_CUBIC); ;
+
+                //Tao ten file
+                String ImageName = System.IO.Path.GetFileNameWithoutExtension(ImagePath);
+                String FileName = String.Format("{0}_face_{1}.jpg", ImageName, FaceID);
+
+                FaceImage.Save(TRAINING_FOLDER_PATH + "/" + FileName);
+
+                StudentImage StuIma = new StudentImage();
+                StuIma.StudentID = StudentID;
+                StuIma.ImageLink = FileName;
+                db.StudentImages.AddObject(StuIma);
+                db.SaveChanges();
+            }
+            Image.Dispose();
+        }
+
+        public static void SaveTrainingData(string ImagePath, int[] FaceIDs, int[] StudentIDs)
+        {
+            RSMEntities db = new RSMEntities();
+            Image<Gray, byte> Image = new Image<Bgr, byte>(ImagePath).Convert<Gray, byte>();
+
+            var FacesDetected = Image.DetectHaarCascade(Haar, DETECT_SCALE, MIN_NEIGHBOR,
+                                0, new System.Drawing.Size(MIN_SIZE, MIN_SIZE))[0];
+            foreach (int FaceID in FaceIDs)
+            {
+                //Neu ko phai unknow thi moi save, la unknow thi bo qua
+                if (StudentIDs[FaceID] != -1)
+                {
+                    var Face = FacesDetected[FaceID];
+                    Image<Gray, byte> FaceImage = Image.Copy(Face.rect).Resize(TRAINING_DATA_SIZE, TRAINING_DATA_SIZE, INTER.CV_INTER_CUBIC);
+                    FaceImage._EqualizeHist();
+
+                    //Tao ten file
+                    String ImageName = System.IO.Path.GetFileNameWithoutExtension(ImagePath);
+                    String FileName = String.Format("{0}_face_{1}.jpg", ImageName, FaceID);
+
+                    FaceImage.Save(TRAINING_FOLDER_PATH + FileName);
+
+                    ////Luu gia tri ten file, userid xuong db
+                    //TrainingData TraDa = new TrainingData();
+                    //TraDa.UserID = StudentIDs[FaceID];
+                    //TraDa.ImageLink = FileName;
+                    //db.TrainingDatas.AddObject(TraDa);
+                }
+
+            }
+            db.SaveChanges();
+        }
+
+
+        private static FaceRecognizer CreateRollCallRecognizer(int RollCallID)
+        {
+            //Tu rollcall ID, tao face recognizer, train
+            FaceRecognizer FaceRec = new LBPHFaceRecognizer(1, 8, 8, 8, 75);
+
+            List<int> StudentIDs = new List<int>();
+            List<Image<Gray, byte>> StudentImages = new List<Image<Gray, byte>>();
+
+            using (RSMEntities _db = new RSMEntities())
+            {
+                //Load danh sach student cua roll call
+                RollCall RollCall = _db.RollCalls.First(roll => roll.RollCallID == RollCallID);
+                foreach (var Student in RollCall.Students)
+                {
+                    foreach (var Image in Student.StudentImages)
+                    {
+                        //Load ID va anh de train cho bo recognizer
+                        StudentIDs.Add(Image.StudentID);
+                        String TrainingImagePath = TRAINING_FOLDER_PATH + "/" + Image.ImageLink;
+                        Image<Gray, byte> TrainingImage = new Image<Gray, byte>(TrainingImagePath);
+
+                        TrainingImage._EqualizeHist();
+                        StudentImages.Add(TrainingImage);
+
+                    }
+                }
+            }
+
+            FaceRec.Train(StudentImages.ToArray(), StudentIDs.ToArray());
+
+            return FaceRec;
+        }
+
+        private static RecognizerResult RecognizeFromImage(FaceRecognizer FaceRec, String ImagePath)
+        {
+            RecognizerResult Result = new RecognizerResult();
+            //Lay moi ten anh, ko lay toan bo duong dan
+            Result.ImageLink = System.IO.Path.GetFileName(ImagePath);
+
+            //Dua anh vao, dua ket qua ra
+            if (Haar == null)
+            {
+                Haar = new HaarCascade(HAAR_XML_PATH);
+            }
+
+            //Chuyen anh trang den roi bat dau recognize
+            Image<Gray, byte> Image = new Image<Gray, byte>(ImagePath);
+
+            var FacesDetected = Image.DetectHaarCascade(Haar, DETECT_SCALE, MIN_NEIGHBOR,
+                                0, new System.Drawing.Size(MIN_SIZE, MIN_SIZE))[0];
+            foreach (var Face in FacesDetected)
+            {
+                FaceRegion FaceReg = new FaceRegion(Face.rect.X, Face.rect.Y,
+                                Face.rect.Width, Face.rect.Height);
+
+                //Nhan dien face la cua ai.
+                Image<Gray, byte> FaceImage = Image.Copy(Face.rect).Resize(TRAINING_DATA_SIZE,
+                                              TRAINING_DATA_SIZE, INTER.CV_INTER_CUBIC);
+                FaceImage._EqualizeHist();
+                FaceRecognizer.PredictionResult PR = FaceRec.Predict(FaceImage);
+                FaceReg.StudentID = PR.Label;
+                FaceReg.StudentName = GetUserName(PR);
+                Result.FaceList.Add(FaceReg);
+            }
+            return Result;
+        }
+
+        private static string GetUserName(FaceRecognizer.PredictionResult PR)
+        {
+            if (PR.Label == -1)
+            {
+                return "Unknown";
+            }
+            else
+            {
+                using (RSMEntities _db = new RSMEntities())
+                {
+                    var Student = _db.Students.First(s => s.StudentID == PR.Label);
+                    return Student.StudentCode + " - " + Student.FullName;
+                }
+            }
+        }
+
+
+        public static List<RecognizerResult> RecognizeStudentForAttendance(int RollCallID, List<String> ImagePaths)
+        {
+            //Dua ID cua roll call, cac hinh da up, cho ra danh sach ket qua
+            FaceRecognizer FaceRec = CreateRollCallRecognizer(RollCallID);
+            List<RecognizerResult> Results = new List<RecognizerResult>();
+
+            foreach (var ImagePath in ImagePaths)
+            {
+                RecognizerResult Result = RecognizeFromImage(FaceRec, ImagePath);
+                Results.Add(Result);
+            }
+
+            //Dung xong nho dispose cho nhe bo nho
+            FaceRec.Dispose();
+            return Results;
+        }
+        */
+    }
+}
---
+++ Code/RSM/FaceRecAutomationTesting/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FaceRecAutomationTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Automation Test.\n");
+            Console.WriteLine("1. Detect an image for 1000 times, compare result.\n");
+            Console.WriteLine("2. Use 4 different haar XML. Compare performance and accuracy.\n");
+
+            //Run100Times();
+            CompareHaarTimes();
+            Console.ReadLine();
+        }
+
+        static void Run100Times()
+        {
+            FaceBO.SetXMLPath("HaarXML");
+            Console.WriteLine("We choose a image with many face for testing. Enter the image path: ");
+            String ImagePath = @"C:\Users\Hoang\Documents\Visual Studio 2010\Projects\RollSystemMobile\FaceRecAutomationTesting\Sample Images\More than 5\47693_634829683201925_1019415806_n.jpg";
+            Console.WriteLine("Press Enter to begin: ");
+            Console.ReadLine();
+
+            DateTime Start = DateTime.Now;
+            List<RecognizerResult> ResultList = new List<RecognizerResult>();
+            Console.WriteLine("Begin Running:");
+            for (int i = 0; i < 100; i++)
+            {
+                var RecognizerResult = FaceBO.DetectFromImage(ImagePath);
+                Console.WriteLine((i + 1) + " detect: ");
+                Console.WriteLine(RecognizerResult.SimpleDetectResult());
+                ResultList.Add(RecognizerResult);
+            }
+
+            TimeSpan TimeDiff = DateTime.Now - Start;
+            Console.WriteLine("The time to run 100 detecs is: " + TimeDiff.TotalMilliseconds/1000 + " seconds");
+
+            bool DifferentResult = false;
+            for (int i = 1; i < ResultList.Count; i++)
+            {
+                if (!ResultList.ElementAt(i).Equals(ResultList.ElementAt(i-1)))
+                {
+                    DifferentResult = true;
+                }
+            }
+            Console.WriteLine("The detect results are different: " + DifferentResult);
+        }
+
+        static void CompareHaarTimes()
+        {
+            FaceBO_HaarChangable.SetXMLPath("HaarXML");
+            Console.WriteLine("For Face Detection. We have 4 haarXML file.\nWe will check the time to detect face with each file.");
+            Console.WriteLine("We will test with a folder contain 100 images.\n");
+            String FolderPath = @"C:\Users\Hoang\Documents\Visual Studio 2010\Projects\RollSystemMobile\FaceRecAutomationTesting\Sample Images";
+            Console.WriteLine("Press Enter to begin: ");
+            Console.ReadLine();
+
+
+            List<HaarType> HaarTypes = new List<HaarType>();
+            HaarTypes.Add(HaarType.AltTree);
+            HaarTypes.Add(HaarType.Alt1);
+            HaarTypes.Add(HaarType.Alt2);
+            HaarTypes.Add(HaarType.Default);
+
+            foreach (var Type in HaarTypes)
+            {
+                FaceBO_HaarChangable.Haar = FaceBO_HaarChangable.CreateHaar(Type);
+                Console.WriteLine("Begin testing for Haar " + Type + ":\n");
+                DateTime Start = DateTime.Now;
+                int index = 1;
+                foreach (var FilePath in Directory.GetFiles(FolderPath, "*.jpg"))
+                {
+                    FaceBO_HaarChangable.DetectFromImage(FilePath);
+                    Console.WriteLine("\tDetect " + index + " images.");
+                    index++;
+                }
+
+                TimeSpan TimeDiff = DateTime.Now - Start;
+                Console.WriteLine("Time to run 100 detecs is: " + TimeDiff.TotalMilliseconds / 1000 + " seconds");
+                Console.WriteLine("Press enter to continue: ");
+                Console.ReadLine();
+            }
+
+            
+        }
+
+    }
+}
