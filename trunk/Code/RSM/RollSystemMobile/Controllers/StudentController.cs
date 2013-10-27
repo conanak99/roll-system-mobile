7b8f730214b0f24b889eee1d4f082a5019f76fe8
huynq60551@fpt.edu.vn <huynq60551@fpt.edu.vn@53922ad7-7c9d-c338-3f44-60befd1a0cec>
2013-10-27 15:11:21 +0000

    edit detail roll call, class

--- trunk/Code/RSM/RollSystemMobile/Controllers/StudentController.cs
+++ trunk/Code/RSM/RollSystemMobile/Controllers/StudentController.cs
@@ -6,26 +6,136 @@ using System.Web.Mvc;
 using RollSystemMobile.Models;
 using RollSystemMobile.Models.BusinessObject;
 using RollSystemMobile.Models.ViewModels;
+using System.Data;
+using System.Data.Entity;
 
 namespace RollSystemMobile.Controllers
 {
     public class StudentController : Controller
     {
-        private StudentBusiness StuBO;
-        private AccountBusiness AccBO;
-        private RollCallBusiness RollBO;
-        //
-        // GET: /Student/
+        private ClassBusiness ClassBO;
+        SelectListFactory slFactory;
+        StudentBusiness StuBO;
+		AccountBusiness AccBO;	
 
         public StudentController()
         {
-            RSMEntities db = new RSMEntities();
-            StuBO = new StudentBusiness(db);
-            AccBO = new AccountBusiness(db);
-            RollBO = new RollCallBusiness(db);
+            slFactory = new SelectListFactory();
+            ClassBO = new ClassBusiness();
+            StuBO = new StudentBusiness();
+			AccBO = new AccountBusiness(db);
+        }
+        //
+        // GET: /Student/
+
+        public ViewResult Index()
+        {
+
+            return View(StuBO.GetAllStudents().ToList());
         }
 
-        //Hien thi cac mon ma student do da hoc
+        //
+        // GET: /Student/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Student student = StuBO.GetStudentByID(id);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.ClassID = slFactory.MakeSelectList<Class>("ClassID", "ClassName");
+            ViewBag.UserID = slFactory.MakeSelectList<User>("UserID", "Username");
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                StuBO.Insert(student);
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = slFactory.MakeSelectList<Class>("ClassID", "ClassName");
+            ViewBag.UserID = slFactory.MakeSelectList<User>("UserID", "Username");
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Student student = StuBO.GetStudentByID(id);
+            ViewBag.ClassID = slFactory.MakeSelectList<Class>("ClassID", "ClassName");
+            ViewBag.UserID = slFactory.MakeSelectList<User>("UserID", "Username");
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                StuBO.Update(student);
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClassID = slFactory.MakeSelectList<Class>("ClassID", "ClassName");
+            ViewBag.UserID = slFactory.MakeSelectList<User>("UserID", "Username");
+            return View(student);
+        }
+
+        public ActionResult ChangeClass(int id, int classid)
+        {
+            var type = ClassBO.GetClassByID(classid).MajorID;
+            List<Class> cls = ClassBO.GetAllClasses().Where(a => a.MajorID == type && a.Students.Count() < 30 && a.ClassID != classid).ToList();
+            ViewBag.ClassID = cls;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangeClass(Student student,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Student std = StuBO.GetStudentByID(id);
+                std.ClassID = student.ClassID;
+                StuBO.UpdateExist(std);
+                return Redirect("Index");
+            }
+            return View();
+        }
+        //
+        // GET: /Student/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Student student = StuBO.GetStudentByID(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = StuBO.GetStudentByID(id);
+            StuBO.Delete(student);
+            return RedirectToAction("Index");
+        }
+//Hien thi cac mon ma student do da hoc
         public ActionResult CourseList()
         {
             string Username = this.HttpContext.User.Identity.Name;
@@ -64,7 +174,9 @@ namespace RollSystemMobile.Controllers
 
             return PartialView("_RollAttendance", Model);
         }
-
-
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
     }
 }
