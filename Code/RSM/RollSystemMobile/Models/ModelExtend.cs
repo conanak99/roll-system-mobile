d7d537f428c895b3a598bcd0241891bbc136599f
huynq60551@fpt.edu.vn <huynq60551@fpt.edu.vn@53922ad7-7c9d-c338-3f44-60befd1a0cec>
2013-12-07 18:13:39 +0000

    update validate

--- Code/RSM/RollSystemMobile/Models/ModelExtend.cs
+++ Code/RSM/RollSystemMobile/Models/ModelExtend.cs
@@ -32,6 +32,57 @@ namespace RollSystemMobile.Models
         public String Phone { get; set; }
     }
 
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+    }
+    public class StudentMetaData
+    {
+        [Required(ErrorMessage = "Please choose a class.")]
+        public int ClassID { get; set; }
+
+        [Required(ErrorMessage = "Please enter student name")]
+        [MinLength(1, ErrorMessage = "Name must be longer than 1 characters")]
+        [MaxLength(50, ErrorMessage = "Name must be shorter than 50 character")]
+        public String FullName { get; set; }
+
+        [Required(ErrorMessage = "Please enter student code")]
+        [MinLength(1, ErrorMessage = "Student code must be longer than 1 characters")]
+        [MaxLength(7, ErrorMessage = "Student code must be shorter than 7 character")]
+        public String StudentCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter CitizenID")]
+        [RegularExpression("[0-9]{9}", ErrorMessage = "Invalid phone number format")]
+        public String CitizenID { get; set; }
+    }
+
+    [MetadataType(typeof(SubjectMetaData))]
+    public partial class Subject
+    {
+    }
+    public class SubjectMetaData
+    {
+        [MinLength(1, ErrorMessage = "Short name must be longer than 1 characters")]
+        [MaxLength(10, ErrorMessage = "Short name must be shorter than 10 character")]
+        public String ShortName { get; set; }
+
+        [Required(ErrorMessage = "Please enter subject full name")]
+        [MinLength(1, ErrorMessage = "Full name must be longer than 1 characters")]
+        [MaxLength(50, ErrorMessage = "Full name must be shorter than 50 character")]
+        public String FullName { get; set; }
+    }
+    [MetadataType(typeof(ClassMetaData))]
+    public partial class Class
+    {
+    }
+    public class ClassMetaData
+    {
+        [Required(ErrorMessage = "Please enter Class name")]
+        [MinLength(5, ErrorMessage = "Name must be longer than 5 characters")]
+        [MaxLength(50, ErrorMessage = "Name must be shorter than 50 character")]
+        public String ClassName { get; set; }
+    }
+
     public partial class RollCall
     {
         public StudySession SessionToCheckAttendance { get; set; }
